792f21da5d5f113927bc5088263323ad475cb512
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-02-09 21:41:08 +0330

    5.2.1

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -1,28 +1,28 @@
-var express = require("express");
-var url = require("url");
-var bodyParser = require('body-parser');
-var randomstring = require("randomstring");
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var querystring = require('querystring');
-var __ = require('underscore');
-__.string = require('underscore.string');
+var express = require('express')
+var url = require('url')
+var bodyParser = require('body-parser')
+var randomstring = require('randomstring')
+var cons = require('consolidate')
+var nosql = require('nosql').load('database.nosql')
+var querystring = require('querystring')
+var __ = require('underscore')
+__.string = require('underscore.string')
 
-var app = express();
+var app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for the token endpoint)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true})) // support form-encoded bodies (for the token endpoint)
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/authorizationServer');
-app.set('json spaces', 4);
+app.engine('html', cons.underscore)
+app.set('view engine', 'html')
+app.set('views', 'files/authorizationServer')
+app.set('json spaces', 4)
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
-};
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+  tokenEndpoint: 'http://localhost:9001/token'
+}
 
 // client information
 var clients = [
@@ -30,76 +30,82 @@ var clients = [
   /*
    * Enter client information here
    */
-];
+  {
+    'client_id': 'oauth-client-1',
+    'client_secret': 'oauth-client-secret-1',
+    'redirect_uris': ['http://localhost:9000/callback']
+  }
+]
 
-var codes = {};
+var codes = {}
 
-var requests = {};
+var requests = {}
 
-var getClient = function(clientId) {
-	return __.find(clients, function(client) { return client.client_id == clientId; });
-};
+var getClient = function (clientId) {
+  return __.find(clients, function (client) { return client.client_id == clientId })
 
-app.get('/', function(req, res) {
-	res.render('index', {clients: clients, authServer: authServer});
-});
+}
 
-app.get("/authorize", function(req, res){
-	
-	/*
-	 * Process the request, validate the client, and send the user to the approval page
-	 */
-	
-});
+app.get('/', function (req, res) {
+  res.render('index', {clients: clients, authServer: authServer})
+})
 
-app.post('/approve', function(req, res) {
+app.get('/authorize', function (req, res) {
 
-	/*
-	 * Process the results of the approval page, authorize the client
-	 */
-	
-});
+  /*
+   * Process the request, validate the client, and send the user to the approval page
+   */
 
-app.post("/token", function(req, res){
+})
 
-	/*
-	 * Process the request, issue an access token
-	 */
+app.post('/approve', function (req, res) {
 
-});
+  /*
+   * Process the results of the approval page, authorize the client
+   */
 
-var buildUrl = function(base, options, hash) {
-	var newUrl = url.parse(base, true);
-	delete newUrl.search;
-	if (!newUrl.query) {
-		newUrl.query = {};
-	}
-	__.each(options, function(value, key, list) {
-		newUrl.query[key] = value;
-	});
-	if (hash) {
-		newUrl.hash = hash;
-	}
-	
-	return url.format(newUrl);
-};
+})
 
-var decodeClientCredentials = function(auth) {
-	var clientCredentials = new Buffer(auth.slice('basic '.length), 'base64').toString().split(':');
-	var clientId = querystring.unescape(clientCredentials[0]);
-	var clientSecret = querystring.unescape(clientCredentials[1]);	
-	return { id: clientId, secret: clientSecret };
-};
+app.post('/token', function (req, res) {
 
-app.use('/', express.static('files/authorizationServer'));
+  /*
+   * Process the request, issue an access token
+   */
+
+})
+
+var buildUrl = function (base, options, hash) {
+  var newUrl = url.parse(base, true)
+  delete newUrl.search
+  if (!newUrl.query) {
+    newUrl.query = {}
+  }
+  __.each(options, function (value, key, list) {
+    newUrl.query[key] = value
+  })
+  if (hash) {
+    newUrl.hash = hash
+  }
+
+  return url.format(newUrl)
+}
+
+var decodeClientCredentials = function (auth) {
+  var clientCredentials = new Buffer(auth.slice('basic '.length), 'base64').toString().split(':')
+  var clientId = querystring.unescape(clientCredentials[0])
+  var clientSecret = querystring.unescape(clientCredentials[1])
+  return {id: clientId, secret: clientSecret}
+}
+
+app.use('/', express.static('files/authorizationServer'))
 
 // clear the database
-nosql.clear();
+nosql.clear()
 
 var server = app.listen(9001, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('OAuth Authorization Server is listening at http://%s:%s', host, port);
-});
+  console.log('OAuth Authorization Server is listening at http://%s:%s', host, port)
+})
  
