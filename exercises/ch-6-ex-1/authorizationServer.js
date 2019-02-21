aae8a1845190980997c69f1b43d1c1cc617b0d5e
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-02-21 10:59:50 +0330

    6.1.1

--- exercises/ch-6-ex-1/authorizationServer.js
+++ exercises/ch-6-ex-1/authorizationServer.js
@@ -121,8 +121,8 @@ app.post('/approve', function (req, res) {
       })
       res.redirect(urlParsed)
       return
-
     } else if (query.response_type == 'token') {
+
       var rscope = getScopesFromForm(req.body)
       var client = getClient(query.client_id)
       var cscope = client.scope ? client.scope.split(' ') : undefined
@@ -134,9 +134,21 @@ app.post('/approve', function (req, res) {
         res.redirect(urlParsed)
         return
       }
+      var access_token = randomstring.generate()
+      nosql.insert({access_token: access_token, client_id: client.client_id, scope: rscope})
 
+      var token_response = {access_token: access_token, token_type: 'Bearer', scope: rscope.join(' ')}
+      if (query.state) {
+        token_response.state = query.state
+      }
+
+      var urlParsed = buildUrl(query.redirect_uri,
+        {},
+        qs.stringify(token_response)
+      )
+      res.redirect(urlParsed)
+      return
     } else {
-
       // we got a response type we don't understand
       var urlParsed = buildUrl(query.redirect_uri, {
         error: 'unsupported_response_type'
@@ -144,6 +156,7 @@ app.post('/approve', function (req, res) {
       res.redirect(urlParsed)
       return
     }
+
   } else {
     // user denied access
     var urlParsed = buildUrl(query.redirect_uri, {
