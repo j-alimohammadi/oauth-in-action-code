4e94e331fe20a5da6790471d510633f650ef74aa
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-02-22 10:16:53 +0330

    6.2

--- exercises/ch-6-ex-2/client.js
+++ exercises/ch-6-ex-2/client.js
@@ -1,99 +1,112 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var request = require("sync-request");
-var url = require("url");
-var qs = require("qs");
-var querystring = require('querystring');
-var cons = require('consolidate');
-var randomstring = require("randomstring");
-var base64url = require('base64url');
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('sync-request')
+var url = require('url')
+var qs = require('qs')
+var querystring = require('querystring')
+var cons = require('consolidate')
+var randomstring = require('randomstring')
+var base64url = require('base64url')
 
-var app = express();
+var app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/client');
+app.engine('html', cons.underscore)
+app.set('view engine', 'html')
+app.set('views', 'files/client')
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
-};
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+  tokenEndpoint: 'http://localhost:9001/token'
+}
 
 // client information
 
 var client = {
-	"client_id": "oauth-client-1",
-	"client_secret": "oauth-client-secret-1",
-	"scope": "foo bar"
-};
+  'client_id': 'oauth-client-1',
+  'client_secret': 'oauth-client-secret-1',
+  'scope': 'foo bar'
+}
 
 //var client = {};
 
-var protectedResource = 'http://localhost:9002/resource';
+var protectedResource = 'http://localhost:9002/resource'
 
-var state = null;
+var state = null
 
-var access_token = null;
-var scope = null;
+var access_token = null
+var scope = null
 
 app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope});
-});
+  res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope})
+})
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', function (req, res) {
 
-	access_token = null;
-	scope = null;
-	
-	/*
-	 * Implement the client credentials flow here
-	 */
-	
-});
+  access_token = null
+  scope = null
 
-app.get('/fetch_resource', function(req, res) {
+  /*
+   * Implement the client credentials flow here
+   */
+  var form_data = qs.stringify({
+    grant_type: 'client_credentials',
+    scope: client.scope
+  })
+  var headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + encodeClientCredentials(client.client_id,
+      client.client_secret)
+  }
+  var tokRes = request('POST', authServer.tokenEndpoint, {
+    body: form_data,
+    headers: headers
+  })
 
-	if (!access_token) {
-		res.render('error', {error: 'Missing access token.'});
-		return;
-	}
-	
-	console.log('Making request with access token %s', access_token);
-	
-	var headers = {
-		'Authorization': 'Bearer ' + access_token,
-		'Content-Type': 'application/x-www-form-urlencoded'
-	};
-	
-	var resource = request('POST', protectedResource,
-		{headers: headers}
-	);
-	
-	if (resource.statusCode >= 200 && resource.statusCode < 300) {
-		var body = JSON.parse(resource.getBody());
-		res.render('data', {resource: body});
-		return;
-	} else {
-		access_token = null;
-		res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
-		return;
-	}
-	
-});
+})
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
-};
+app.get('/fetch_resource', function (req, res) {
 
-app.use('/', express.static('files/client'));
+  if (!access_token) {
+    res.render('error', {error: 'Missing access token.'})
+    return
+  }
+
+  console.log('Making request with access token %s', access_token)
+
+  var headers = {
+    'Authorization': 'Bearer ' + access_token,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  var resource = request('POST', protectedResource,
+    {headers: headers}
+  )
+
+  if (resource.statusCode >= 200 && resource.statusCode < 300) {
+    var body = JSON.parse(resource.getBody())
+    res.render('data', {resource: body})
+    return
+  } else {
+    access_token = null
+    res.render('error', {error: 'Server returned response code: ' + resource.statusCode})
+    return
+  }
+
+})
+
+var encodeClientCredentials = function (clientId, clientSecret) {
+  return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64')
+}
+
+app.use('/', express.static('files/client'))
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
-});
+  var host = server.address().address
+  var port = server.address().port
+  console.log('OAuth Client is listening at http://%s:%s', host, port)
+})
  
